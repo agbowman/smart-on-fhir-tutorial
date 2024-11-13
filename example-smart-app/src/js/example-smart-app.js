ee5e5c47ec61598877221a25d9b9de9c63574c75
agbowman <alex.bowman@baystatehealth.org>
2024-11-13 16:41:51 -0500

    trying to add clin doc

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,5 +1,5 @@
-(function(window){
-  window.extractData = function() {
+(function (window) {
+  window.extractData = function () {
     var ret = $.Deferred();
 
     function onError() {
@@ -7,24 +7,25 @@
       ret.reject();
     }
 
-    function onReady(smart)  {
+    function onReady(smart) {
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
-                    }
-                  });
+          type: 'Observation',
+          query: {
+            code: {
+              $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                'http://loinc.org|2089-1', 'http://loinc.org|55284-4'
+              ]
+            }
+          }
+        });
 
         $.when(pt, obv).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
+        $.when(pt, obv).done(function (patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
@@ -37,8 +38,8 @@
           }
 
           var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          var systolicbp = getBloodPressureValue(byCodes('55284-4'), '8480-6');
+          var diastolicbp = getBloodPressureValue(byCodes('55284-4'), '8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
 
@@ -49,7 +50,7 @@
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
 
-          if (typeof systolicbp != 'undefined')  {
+          if (typeof systolicbp != 'undefined') {
             p.systolicbp = systolicbp;
           }
 
@@ -72,25 +73,43 @@
 
   };
 
-  function defaultPatient(){
+  function defaultPatient() {
     return {
-      fname: {value: ''},
-      lname: {value: ''},
-      gender: {value: ''},
-      birthdate: {value: ''},
-      height: {value: ''},
-      systolicbp: {value: ''},
-      diastolicbp: {value: ''},
-      ldl: {value: ''},
-      hdl: {value: ''},
+      fname: {
+        value: ''
+      },
+      lname: {
+        value: ''
+      },
+      gender: {
+        value: ''
+      },
+      birthdate: {
+        value: ''
+      },
+      height: {
+        value: ''
+      },
+      systolicbp: {
+        value: ''
+      },
+      diastolicbp: {
+        value: ''
+      },
+      ldl: {
+        value: ''
+      },
+      hdl: {
+        value: ''
+      },
     };
   }
 
   function getBloodPressureValue(BPObservations, typeOfPressure) {
     var formattedBPObservations = [];
-    BPObservations.forEach(function(observation){
-      var BP = observation.component.find(function(component){
-        return component.code.coding.find(function(coding) {
+    BPObservations.forEach(function (observation) {
+      var BP = observation.component.find(function (component) {
+        return component.code.coding.find(function (coding) {
           return coding.code == typeOfPressure;
         });
       });
@@ -105,16 +124,16 @@
 
   function getQuantityValueAndUnit(ob) {
     if (typeof ob != 'undefined' &&
-        typeof ob.valueQuantity != 'undefined' &&
-        typeof ob.valueQuantity.value != 'undefined' &&
-        typeof ob.valueQuantity.unit != 'undefined') {
-          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
+      typeof ob.valueQuantity != 'undefined' &&
+      typeof ob.valueQuantity.value != 'undefined' &&
+      typeof ob.valueQuantity.unit != 'undefined') {
+      return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
     } else {
       return undefined;
     }
   }
 
-  window.drawVisualization = function(p) {
+  window.drawVisualization = function (p) {
     $('#holder').show();
     $('#loading').hide();
     $('#fname').html(p.fname);
@@ -127,5 +146,68 @@
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
   };
+
+
+
+
+
+  ///TESTING CODE VVVVV
+  window.submitClinicalNote = function () {
+    var noteTitle = $('#note-title').val();
+    var noteContent = $('#note-content').val();
+
+    if (!noteTitle || !noteContent) {
+      $('#errors').html('<p>Please fill in all required fields.</p>');
+      return;
+    }
+
+    var documentReference = createDocumentReference(noteTitle, noteContent);
+
+    window.smart.api.create({
+      resourceType: 'DocumentReference',
+      body: documentReference
+    }).then(function (response) {
+      $('#feedback-message').text('Clinical note submitted successfully!');
+      $('#feedback').show();
+      $('#errors').hide();
+      $('#clinical-note-form')[0].reset();
+    }).catch(function (error) {
+      console.error('Error submitting clinical note:', error);
+      $('#errors').html('<p>Error submitting clinical note. Please try again.</p>');
+      $('#feedback').hide();
+    });
+  };
+
+  function createDocumentReference(title, content) {
+    var encodedContent = btoa(unescape(encodeURIComponent(content)));
+
+    return {
+      status: 'current',
+      type: {
+        coding: [{
+          system: 'http://loinc.org',
+          code: '34133-9',
+          display: 'Summarization of episode note'
+        }]
+      },
+      subject: {
+        reference: 'Patient/' + window.smart.patient.id
+      },
+      content: [{
+        attachment: {
+          contentType: 'text/plain',
+          data: encodedContent
+        }
+      }],
+      context: {
+        encounter: {
+          reference: 'Encounter/' + window.smart.encounter.id
+        }
+      }
+    };
+  }
+  ///TESTING CODE ^^^^^
+
+
 
 })(window);
